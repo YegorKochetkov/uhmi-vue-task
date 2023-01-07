506a34f704ed14f79da36982aeb49bd00cb7dc0b
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-01-07 12:01:49 +0200

    feat: update App

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Posts</RouterLink>
+      <RouterLink to="/map">Map</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+nav {
+  width: 100%;
+  font-size: 1rem;
+  text-align: center;
+  margin-bottom: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
