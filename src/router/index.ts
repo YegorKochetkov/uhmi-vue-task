0c3e5defb1c6b5138c9bc5610cd25c927180a6e7
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-01-07 12:01:20 +0200

    feat: add router

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/map",
+      name: "map",
+      component: () => import("../views/MapView.vue"),
+    },
+  ],
+});
+
+export default router;
