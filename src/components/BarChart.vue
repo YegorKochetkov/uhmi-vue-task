ef5311e1c97acb9dd699a18c2c93a39ab6a5cd06
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-01-07 11:53:54 +0200

    feat: add BarChart component

---
+++ src/components/BarChart.vue
@@ -0,0 +1,41 @@
+<template>
+  <Bar :data="chartData" :options="chartOptions" />
+</template>
+
+<script>
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+import { defineComponent } from "vue";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+export default defineComponent({
+  name: "BarChart",
+  components: { Bar },
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+    chartOptions: {
+      type: Object,
+      default: () => {},
+    },
+  },
+});
+</script>
